4243dc48fb23182c530de36a2322cdc3ce5f1b90
satishkr0905 <satishkr0905@gmail.com>
2020-02-10 09:56:32 +0530

    aws region corrected

--- vars.tf
+++ vars.tf
@@ -1,3 +1,3 @@
 variable "AWS_REGION" {    
-    default = "eu-west-2"
+    default = "ap-south-1"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
+# Declare the data source
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
 resource "aws_vpc" "dev-vpc01" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true     # It will provide an internal domain name
@@ -12,7 +18,7 @@ resource "aws_subnet" "PubSN" {
   vpc_id = aws_vpc.dev-vpc01.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true  // it makes subnet public.
-  availability_zone =  "ap-south-1a"
+  availability_zone = data.aws_availability_zones.available.names[0]
   tags = {
       Name = "PubSN"
   }
