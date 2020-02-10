afb627655ce4078a7d958004e5f53f846bc156a6
satishkr0905 <satishkr0905@gmail.com>
2020-02-10 09:33:46 +0530

    adding vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "dev-vpc01" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true     # It will provide an internal domain name
+  enable_dns_hostnames = true   # gives you an internal host name
+  instance_tenancy = "default"
+  tags {
+      Name = dev-vpc01
+  }
+}
+
+resource "aws_subnet" "PubSN" {
+  vpc_id = "${aws_vpc.dev-vpc01.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true  // it makes subnet public.
+  availability_zone =  "ap-south-1a"
+  tags {
+      Name = "PubSN"
+  }
+
+}
+
